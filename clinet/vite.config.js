ec37bf828a3c11b4a0298eeb4833afff40ebfb4a
programminsekbo <rkrafikridoy5887@gmail.com>
2025-01-30 08:08:31 -0800

    fines

--- clinet/vite.config.js
+++ clinet/vite.config.js
@@ -5,10 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api/": {
-        target: "http://localhost:5050",
-      },
+    "/api": {
+      target: "https://new-social-media-ten.vercel.app", 
+      changeOrigin: true,
+      secure: true,
+  
     },
   },
 });
